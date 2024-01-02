52255b214ce466a21daae90553573d17766f7198
Himanshu <hbansal_be21@thapar.edu>
2024-01-03 02:40:46 +0530

    Fixed a bug

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -9,7 +9,7 @@ connect();
 export async function GET(request: NextRequest) {
     try {
         const userId = await getDataFromToken(request);
-        const user = await User.findById({_id: userId}).select("-password");
+        const user = await User.findById({userId}).select("-password");
         return NextResponse.json({
             message: "User found successfully",
             success: true,
